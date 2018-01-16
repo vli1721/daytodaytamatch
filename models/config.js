13809c66098ca41347d44aafbcb69c19760d77de
Elizabeth Yeoh-Wang <eyw410@gmail.com>
2018-01-16 11:52:16 -0500

    setting up ports

--- models/config.js
+++ models/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    port: 5000,
-    dbUrl: 'localhost',
+    port: 3000,
+    dbUrl: 'localhost:5000',
 
     // secret for creating tokens
     token_secret: process.env.TOKEN_SECRET || 'reughdjsasdkpmasipkmsdfadf',
